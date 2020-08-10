cd436b71390aebb2c3487f9234ea86b0333c25f4
Alexis Pascual <ap.alexispascual@gmail.com>
2020-08-10 11:56:48 -0400

    Updating subsystem queries

--- backend/cubesat-appserver/routes/systems.js
+++ backend/cubesat-appserver/routes/systems.js
@@ -9,46 +9,109 @@ router.route('/')
 
     // GET /systems returns all systems
     .get(parseUrlencoded, parseJSON, (req, res) => {
-        try {
-            db.query("SELECT * FROM systems", function (error, results, fields) {
-                if (error) throw error;
-                res.send(results);
-            })
-        } catch (err) {
-            console.log(err);
-            res.send(err);
-        }
+        ;(async () => {
+            const client = await db.connect()
+            try {
+                const query = 'SELECT * FROM subsystems';
+                const response = await client.query(query)
+                res.json(response.rows);
+            } catch (e) {
+                console.log(e);
+                res.send(e);
+            } finally {
+                await client.release();
+            }
+        })().catch(e => console.error(e.stack));
+
+        // try {
+        //     db.query("SELECT * FROM systems", function (error, results, fields) {
+        //         if (error) throw error;
+        //         res.send(results);
+        //     })
+        // } catch (err) {
+        //     console.log(err);
+        //     res.send(err);
+        // }
     })
 
     // POST /systems adds a new system to the systems table
     .post(parseUrlencoded, parseJSON, (req, res) => {
-        try {
-            db.query("INSERT INTO systems (systemName) VALUES (?)", req.body.systemName, function (error, results, fields) {
-                if (error) throw error;
-                console.log(results);
-                res.json({newSys:results.insertId});
-                //res.sendStatus(200);
-            })
-        } catch (err) {
-            console.log(err);
-            res.send(err);
-        }
+        ;(async () => {
+            const client = await db.connect()
+            try {
+                const query = {
+                        text: 
+                            `INSERT INTO subsystems (systemName)
+          
+                            VALUES ($1)
+        
+                            RETURNING *
+                            `,
+                            values: [req.body.systemName],
+                        }
+                const response = await client.query(query)
+                res.json({newSys: response.rows});
+            } catch (e) {
+                console.log(e);
+                res.send(e);
+            } finally {
+                await client.release();
+            }
+        })().catch(e => console.error(e.stack));
+
+        // try {
+        //     db.query("INSERT INTO systems (systemName) VALUES (?)", req.body.systemName, function (error, results, fields) {
+        //         if (error) throw error;
+        //         console.log(results);
+        //         res.json({newSys:results.insertId});
+        //         //res.sendStatus(200);
+        //     })
+        // } catch (err) {
+        //     console.log(err);
+        //     res.send(err);
+        // }
     });
 
 router.route('/:ID')
     // PUT /systems/:systemID updates a system name
     .put(parseUrlencoded, parseJSON, (req, res) => {
-        try {
-            var updateParams = [req.body.systemName, req.params.ID];
-            db.query("UPDATE systems SET systemName = ? WHERE systemID = ?", updateParams, function (error, results, fields) {
-                if (error) throw error;
-                res.json({updateSys:results.insertId});
-                //res.sendStatus(200);
-            })
-        } catch (err) {
-            console.log(err);
-            res.send(err);
-        }
+        ;(async () => {
+            const client = await db.connect()
+            try {
+                const query = {
+                        text: 
+                            `UPDATE subsystems 
+                            
+                            SET subsystemName = $1,
+        
+                            WHERE
+                                subsystemID = $2
+
+                            RETURNING *
+                          `,
+                          values: [req.body.systemName, req.params.ID],
+                        }
+                const response = await client.query(query)
+                res.json({updateSys: response.rows});
+            } catch (e) {
+                console.log(e);
+                res.send(e);
+            } finally {
+                await client.release();
+            }
+        })().catch(e => console.error(e.stack));
+        
+        // try {
+        //     var updateParams = [req.body.systemName, req.params.ID];
+        //     db.query("UPDATE systems SET systemName = ? WHERE systemID = ?", updateParams, function (error, results, fields) {
+        //         if (error) throw error;
+        //         res.json({updateSys:results.insertId});
+        //         //res.sendStatus(200);
+        //     })
+        // } catch (err) {
+        //     console.log(err);
+        //     res.send(err);
+        // }
     })
 
     // DELETE /systems/:systemID deletes a system, its components, and those components' componentTelemetries
