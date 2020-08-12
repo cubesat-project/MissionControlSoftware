346a6db38a953680f90a593cbdebdf6fdf73adc7
Alexis Pascual <ap.alexispascual@gmail.com>
2020-08-12 10:49:13 -0400

    Editting presetTelecommands

--- backend/cubesat-appserver/routes/presetTelecommands.js
+++ backend/cubesat-appserver/routes/presetTelecommands.js
@@ -7,64 +7,184 @@ var db = require('../database');
 
 router.route('/')
     .post(parseUrlencoded, parseJSON, (req, res) => {
-        try {
-            var insertParameters = [req.body.telecommandID, req.body.batchID, req.body.secondDelay, req.body.minuteDelay, req.body.hourDelay, req.body.dayDelay, req.body.priorityLevel, req.body.commandParameters];
+        ;(async () => {
+            const client = await db.connect()
+            try {
+                const query = {
+                        text: 
+                            `INSERT INTO "presetTelecommands" ("telecommandID", 
+                                                               "batchID", 
+                                                               "secondDelay", 
+                                                               "minuteDelay", 
+                                                               "hourDelay", 
+                                                               "dayDelay", 
+                                                               "priorityLevel", 
+                                                               "commandParameters")
+          
+                            VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+        
+                            RETURNING *
+                            `,
+                            values: [req.body.telecommandID, 
+                                     req.body.batchID, 
+                                     req.body.secondDelay, 
+                                     req.body.minuteDelay, 
+                                     req.body.hourDelay, 
+                                     req.body.dayDelay, 
+                                     req.body.priorityLevel, 
+                                     req.body.commandParameters]
+                        }
+                const response = await client.query(query)
+                res.json(response.rows);
+            } catch (e) {
+                console.log(e);
+                res.send(e);
+            } finally {
+                await client.release();
+            }
+        })().catch(e => console.error(e.stack));
 
-            // using this pattern of using ? in our query builder does the escaping for us! No need to worry about sql injection
-            db.query('INSERT INTO presetTelecommands(telecommandID, batchID, secondDelay, minuteDelay, hourDelay, dayDelay, priorityLevel, commandParameters) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', insertParameters, function (error, results, fields) {
-                if (error) throw error;
+        // try {
+        //     var insertParameters = [req.body.telecommandID, req.body.batchID, req.body.secondDelay, req.body.minuteDelay, req.body.hourDelay, req.body.dayDelay, req.body.priorityLevel, req.body.commandParameters];
 
-                res.json(results);
-              });
-        } catch (err) {
-            console.log(err);
-            res.send(err);
-        }
+        //     // using this pattern of using ? in our query builder does the escaping for us! No need to worry about sql injection
+        //     db.query('INSERT INTO presetTelecommands(telecommandID, batchID, secondDelay, minuteDelay, hourDelay, dayDelay, priorityLevel, commandParameters) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', insertParameters, function (error, results, fields) {
+        //         if (error) throw error;
+
+        //         res.json(results);
+        //       });
+        // } catch (err) {
+        //     console.log(err);
+        //     res.send(err);
+        // }
     });
 
-router.route('/:id')
+router.route('/:ID')
     .get(parseUrlencoded, parseJSON, (req, res) => {
-        try {
-            var batchID = req.params.id;
-            
-            db.query('SELECT presetTelecommands.*, telecommands.name from presetTelecommands JOIN telecommands on presetTelecommands.telecommandID = telecommands.telecommandID WHERE batchID = ? ORDER BY dayDelay, hourDelay, minuteDelay, secondDelay', batchID, function (error, results, fields) {
-                if (error) throw error;
+        ;(async () => {
+            const client = await db.connect()
+            try {
+                const query = {
+                        text: `SELECT presetTelecommands.*, telecommands.name FROM presetTelecommands 
 
-                res.json(results);
-            });
-        } catch (err) {
-            console.log(err);
-            res.send(err);
-        }
+                                JOIN telecommands ON presetTelecommands.telecommandID = telecommands.telecommandID 
+
+                                WHERE batchID = ? 
+
+                                ORDER BY dayDelay, hourDelay, minuteDelay, secondDelay`,
+
+                        values: [req.params.id]
+                        }
+                const response = await client.query(query)
+                res.json(response.rows);
+            } catch (e) {
+                console.log(e);
+                res.send(e);
+            } finally {
+                await client.release();
+            }
+        })().catch(e => console.error(e.stack));
+
+        // try {
+        //     var batchID = req.params.id;
+            
+        //     db.query('SELECT presetTelecommands.*, telecommands.name from presetTelecommands JOIN telecommands on presetTelecommands.telecommandID = telecommands.telecommandID WHERE batchID = ? ORDER BY dayDelay, hourDelay, minuteDelay, secondDelay', batchID, function (error, results, fields) {
+        //         if (error) throw error;
+
+        //         res.json(results);
+        //     });
+        // } catch (err) {
+        //     console.log(err);
+        //     res.send(err);
+        // }
     })
     .put(parseUrlencoded, parseJSON, (req, res) => {
-        try {
-            var presetTelecommandID = req.params.id;
-            var updateParams = [req.body.secondDelay, req.body.minuteDelay, req.body.hourDelay, req.body.dayDelay, req.body.priorityLevel, req.body.commandParameters, presetTelecommandID];
-            
-            db.query('UPDATE presetTelecommands SET secondDelay = ?, minuteDelay = ?, hourDelay = ?, dayDelay = ?, priorityLevel = ?, commandParameters = ? WHERE presetTelecommandID = ?;', updateParams, function (error, results, fields) {
-                if (error) throw error;
+        ;(async () => {
+            const client = await db.connect()
+            try {
+                const query = {
+                        text: 
+                            `UPDATE "presetTelecommands" 
+                            
+                            SET "secondDelay" = $1,
+                                "minuteDelay" = $2,
+                                "hourDelay" = $3,
+                                "dayDelay" = $4,
+                                "priorityLevel" = $5,
+                                "commandParameters" = $6,
+        
+                            WHERE presetTelecommandID = $7
 
-                res.json(results);
-              });
-        } catch (err) {
-            console.log(err);
-            res.send(err);
-        }
+                            RETURNING *
+                          `,
+                          values: [req.body.secondDelay, 
+                                   req.body.minuteDelay, 
+                                   req.body.hourDelay, 
+                                   req.body.dayDelay, 
+                                   req.body.priorityLevel, 
+                                   req.body.commandParameters, 
+                                   req.params.ID]
+                        }
+                const response = await client.query(query)
+                res.json(response.rows);
+            } catch (e) {
+                console.log(e);
+                res.send(e);
+            } finally {
+                await client.release();
+            }
+        })().catch(e => console.error(e.stack));
+
+        // try {
+        //     var presetTelecommandID = req.params.id;
+        //     var updateParams = [req.body.secondDelay, req.body.minuteDelay, req.body.hourDelay, req.body.dayDelay, req.body.priorityLevel, req.body.commandParameters, presetTelecommandID];
+            
+        //     db.query(`UPDATE presetTelecommands SET secondDelay = ?, minuteDelay = ?, hourDelay = ?, dayDelay = ?, priorityLevel = ?, commandParameters = ? WHERE presetTelecommandID = ?;`, updateParams, function (error, results, fields) {
+        //         if (error) throw error;
+
+        //         res.json(results);
+        //       });
+        // } catch (err) {
+        //     console.log(err);
+        //     res.send(err);
+        // }
     })
     .delete(parseUrlencoded, parseJSON, (req, res) => {
-        try {
-            var presetTelecommandID = req.params.id;
-            
-            db.query('DELETE FROM presetTelecommands WHERE presetTelecommandID = ?', presetTelecommandID, function (error, results, fields) {
-                if (error) throw error;
+        ;(async () => {
+            const client = await db.connect()
+            try {
+                const query = {
+                        text: 
+                            `DELETE FROM "presetTelecommands" 
 
-                res.json(results);
-              });
-        } catch (err) {
-            console.log(err);
-            res.send(err);
-        }
+                            WHERE "presetTelecommandID" = $1
+
+                            RETURNING *
+                          `,
+                          values: [req.params.ID]
+                        }
+                const response = await client.query(query)
+                res.json(response.rows);
+            } catch (e) {
+                console.log(e);
+                res.send(e);
+            } finally {
+                await client.release();
+            }
+        })().catch(e => console.error(e.stack));
+
+        // try {
+        //     var presetTelecommandID = req.params.id;
+            
+        //     db.query('DELETE FROM presetTelecommands WHERE presetTelecommandID = ?', presetTelecommandID, function (error, results, fields) {
+        //         if (error) throw error;
+
+        //         res.json(results);
+        //       });
+        // } catch (err) {
+        //     console.log(err);
+        //     res.send(err);
+        // }
     });
 
 module.exports = router;
