7e87a9d51faeaa32bcc11967e55f8293c3a5ed08
alexispascual <ap.alexispascual@gmail.com>
2020-09-29 10:30:01 -0400

    Fixing gitignore

--- frontend/mission-ops/src/environments/environment.ts
+++ frontend/mission-ops/src/environments/environment.ts
@@ -1,13 +1,12 @@
-export const environment = {
-    production: false,
-    apiRouteBase: 'http://localhost:3000',
-    cognito: {
-      accessKeyId: 'AKIAQCM2BA4DZ4B3GDOV',
-      secretAccessKey: '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',
-      region: 'us-east-2',
-      userPoolId: 'us-east-2_YZSlXzFlB',
-      userPoolClientId: '1bn896ocv98neen3r2djed4r7i'
-    },
-    groundStationIP: 'http://0.0.0.0:3700'
-  };
-  
+export const environment = { 
+  production: false, 
+  apiRouteBase: 'http://localhost:3000', 
+  cognito: { 
+    accessKeyId: 'AKIAQCM2BA4DZ4B3GDOV', 
+    secretAccessKey: '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', 
+    region: 'us-east-2', 
+    userPoolId: 'us-east-2_YZSlXzFlB', 
+    userPoolClientId: '1bn896ocv98neen3r2djed4r7i' 
+  }, 
+  groundStationIP: 'http://0.0.0.0:3700' 
+}; 
